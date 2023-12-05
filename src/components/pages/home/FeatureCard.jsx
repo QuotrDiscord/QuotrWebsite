c139aafe93caffe4f9614b1ee5a8ba3e2e55d1ff
H-274 <GitHub@jsmarier.com>
2023-12-05 18:15:18 -0500

    Started work on feature card

--- src/components/pages/home/FeatureCard.jsx
+++ src/components/pages/home/FeatureCard.jsx
@@ -1,3 +1,17 @@
-export const FeatureCard = () => {
-  return <div></div>;
+export const FeatureCard = ({ name, icon, description, learnMoreUrl }) => {
+  return (
+    <div className="flex flex-col gap-4 p-5 text-white bg-black bg-opacity-25 font-comfortaa rounded-xl">
+      <div className="flex flex-row justify-between">
+        <p className="text-2xl font-bold">{name}</p>
+        <img src={icon} />
+      </div>
+      <p className="text-xl font-light">{description}</p>
+      <a
+        className="text-sm font-light text-right underline"
+        href={learnMoreUrl}
+      >
+        Learn more...
+      </a>
+    </div>
+  );
 };
--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -4,32 +4,32 @@ import featureCardData from "../../../data/featureCardData.json";
 
 export const HomePage = () => {
   return (
-    <div className='flex w-full bg-gradient-to-r from-fuchsia-950 to-green-950 grow'>
-      <div className='flex grow bg-gradient-to-b from-neutral-900 to-transparent'>
-        <div className='flex flex-col items-center justify-center gap-24 grow pt-14'>
-          <div className='flex flex-col items-center gap-5'>
-            <h1 className='text-transparent text-9xl font-julius-sans bg-gradient-to-r from-purple-500 to-green-500 bg-clip-text'>
+    <div className="flex w-full bg-gradient-to-r from-violet-950 to-green-950 grow">
+      <div className="flex grow bg-gradient-to-b from-neutral-900 to-transparent">
+        <div className="flex flex-col items-center justify-center gap-24 grow pt-14">
+          <div className="flex flex-col items-center gap-5">
+            <h1 className="text-transparent text-9xl font-julius-sans bg-gradient-to-r from-violet-600 to-green-600 bg-clip-text">
               Quotr
             </h1>
-            <div className='p-1 transition-all group hover:-translate-y-0.5 hover:shadow-2xl w-min rounded-xl bg-gradient-to-r from-purple-500 to-green-500'>
-              <button className='p-6 transition-all rounded-lg bg-neutral-900 '>
-                <p className='text-4xl font-bold text-white transition-colors font-comfortaa whitespace-nowrap'>
+            <div className="p-1 transition-all group hover:-translate-y-0.5 hover:shadow-2xl w-min rounded-xl bg-gradient-to-r from-violet-600 to-green-600">
+              <button className="p-6 transition-all rounded-lg bg-neutral-900 ">
+                <p className="text-4xl font-bold text-white transition-colors font-comfortaa whitespace-nowrap">
                   Get Started
                 </p>
               </button>
             </div>
-            <div className='inline-flex items-center justify-center gap-16 max-w-7xl'>
-              {featureCardData.map((x) => {
-                return (
-                  <FeatureCard
-                    name={x.name}
-                    icon={x.icon}
-                    description={x.icon}
-                    learnMoreUrl={x.icon}
-                  />
-                );
-              })}
-            </div>
+          </div>
+          <div className="inline-flex items-center justify-center gap-16 max-w-7xl">
+            {featureCardData.map((x) => {
+              return (
+                <FeatureCard
+                  name={x.name}
+                  icon={x.icon}
+                  description={x.description}
+                  learnMoreUrl={x.icon}
+                />
+              );
+            })}
           </div>
         </div>
       </div>

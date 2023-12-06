1b0ef962bf8774e8db25ee7a77cdb6f0b9489f13
H-274 <GitHub@jsmarier.com>
2023-12-05 19:56:50 -0500

    Added card icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,21 @@ import { NavbarItem } from "./NavbarItem";
 
 export const Navbar = () => {
   return (
-    <div className='flex justify-center bg-neutral-900'>
-      <div className='grid items-center justify-between w-full grid-cols-2 px-12 text-white border-b border-white h-14 xl:grid-cols-3 flex-grid'>
-        <h1 className='text-4xl font-julius-sans'>Quotr</h1>
-        <ul className='flex-row items-center justify-center hidden w-full h-full gap-10 text-lg font-comfortaa xl:flex'>
+    <div className="flex justify-center bg-neutral-900">
+      <div className="grid items-center justify-between w-full grid-cols-2 px-12 text-white border-b border-white h-14 xl:grid-cols-3 flex-grid">
+        <h1 className="text-4xl font-julius-sans">Quotr</h1>
+        <ul className="flex-row items-center justify-center hidden w-full h-full gap-10 text-lg font-comfortaa xl:flex">
           <NavbarItem name={"Home"} />
           <NavbarItem name={"Dashboard"} />
           <NavbarItem name={"Documentation"} />
           <NavbarItem name={"About"} />
         </ul>
-        <div className=' justify-self-end'>ICON</div>
+        <div className="justify-self-end">
+          <img
+            className="p-1 border-2 border-white rounded-full w-9 h-9"
+            src="/icons/user.svg"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/pages/home/FeatureCard.jsx
+++ src/components/pages/home/FeatureCard.jsx
@@ -1,17 +1,17 @@
 export const FeatureCard = ({ name, icon, description, learnMoreUrl }) => {
   return (
-    <div className="flex flex-col gap-4 p-5 text-white bg-black bg-opacity-25 font-comfortaa rounded-xl">
-      <div className="flex flex-row justify-between">
+    <li className="flex flex-col gap-4 p-5 text-white bg-black bg-opacity-25 font-comfortaa rounded-xl min-w-[300px] max-w-xs">
+      <div className="flex flex-row items-center justify-between">
         <p className="text-2xl font-bold">{name}</p>
         <img src={icon} />
       </div>
-      <p className="text-xl font-light">{description}</p>
+      <p className="text-xl font-light line-clamp-4">{description}</p>
       <a
         className="text-sm font-light text-right underline"
         href={learnMoreUrl}
       >
         Learn more...
       </a>
-    </div>
+    </li>
   );
 };

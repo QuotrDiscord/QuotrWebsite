77e4589e06d115df26ecb1ee5210063300f0ffd3
H-274 <GitHub@jsmarier.com>
2023-12-06 11:32:12 -0500

    Replaced old gradient references with new css class

--- src/pages/errors/NotFoundPage.jsx
+++ src/pages/errors/NotFoundPage.jsx
@@ -6,18 +6,18 @@ export const NotFoundPage = () => {
 
   return (
     <PageBg>
-      <div className="flex flex-col items-center w-full gap-12 p-24 text-lg text-white font-comfortaa">
-        <div className="flex flex-col items-center">
-          <h1 className="text-transparent text-9xl font-julius-sans bg-gradient-to-r from-violet-600 to-green-600 bg-clip-text">
+      <div className='flex flex-col items-center w-full gap-12 p-24 text-lg text-white font-comfortaa'>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-transparent text-9xl font-julius-sans bg-gradient-to-r main-gradient-light bg-clip-text'>
             404
           </h1>
           <p>Looks like something went wrong!</p>
         </div>
-        <div className="text-center">
+        <div className='text-center'>
           <p>The following route could not be found:</p>
           <p>{location.pathname}</p>
         </div>
-        <div className="grow" />
+        <div className='grow' />
         <p>If you believe this is a mistake, submit an issue on GitHub</p>
       </div>
     </PageBg>
--- src/pages/errors/UnauthorizedPage.jsx
+++ src/pages/errors/UnauthorizedPage.jsx
@@ -6,18 +6,18 @@ export const UnauthorizedPage = () => {
 
   return (
     <PageBg>
-      <div className="flex flex-col items-center w-full gap-12 p-24 text-lg text-white font-comfortaa">
-        <div className="flex flex-col items-center">
-          <h1 className="text-transparent text-9xl font-julius-sans bg-gradient-to-r from-violet-600 to-green-600 bg-clip-text">
+      <div className='flex flex-col items-center w-full gap-12 p-24 text-lg text-white font-comfortaa'>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-transparent text-9xl font-julius-sans bg-gradient-to-r main-gradient-light bg-clip-text'>
             401
           </h1>
           <p>Looks like something went wrong!</p>
         </div>
-        <div className="text-center">
+        <div className='text-center'>
           <p>You aren't allowed to access the following page:</p>
           <p>{location.pathname}</p>
         </div>
-        <div className="grow" />
+        <div className='grow' />
         <p>If you believe this is a mistake, submit an issue on GitHub</p>
       </div>
     </PageBg>

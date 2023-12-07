bf03cd58922be2dafddd8f23b2c3f3701338cec9
H-274 <GitHub@jsmarier.com>
2023-12-06 20:05:07 -0500

    Changed media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,11 +6,11 @@ import navbarItems from "../../data/navbarItems.json";
 export const Navbar = ({ userId }) => {
   return (
     <div className="flex justify-center bg-neutral-900">
-      <div className="grid items-center justify-between w-full grid-cols-2 px-12 text-white border-b border-white h-14 xl:grid-cols-3 flex-grid">
+      <div className="grid items-center justify-between w-full grid-cols-2 px-12 text-white border-b border-white lg:grid-cols-3 h-14 flex-grid">
         <h1 className="text-4xl font-julius-sans">
           <A href={"/"}>Quotr</A>
         </h1>
-        <ul className="flex-row items-center justify-center hidden w-full h-full gap-10 text-lg font-comfortaa xl:flex">
+        <ul className="flex-row items-center justify-center hidden w-full h-full gap-5 text-lg lg:flex font-comfortaa">
           {navbarItems.map((x) => {
             if (userId === "" && x.url === "/dash") return;
 
--- src/components/global/PageBg.jsx
+++ src/components/global/PageBg.jsx
@@ -2,8 +2,8 @@ import { children } from "solid-js";
 
 export const PageBg = ({ children }) => {
   return (
-    <div className='flex w-full main-gradient-dark grow bg-gradient-to-b md:bg-gradient-to-r'>
-      <div className='flex grow bg-gradient-to-b from-neutral-900 to-transparent'>
+    <div className="flex w-full main-gradient-dark grow bg-gradient-to-b lg:bg-gradient-to-r">
+      <div className="flex grow bg-gradient-to-b from-neutral-900 to-transparent">
         {children}
       </div>
     </div>

627d21818929730f709706ba0c6b19f446a08dd7
H-274 <GitHub@jsmarier.com>
2023-12-06 20:17:39 -0500

    Moved navbar to page and changed button hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { Route, Routes } from "@solidjs/router";
 import { Show, createSignal } from "solid-js";
 
-import { Navbar } from "./components/Navbar/Navbar";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/errors/NotFoundPage";
 import { UnauthorizedPage } from "./pages/errors/UnauthorizedPage";
+import { DocumentationPage } from "./pages/DocumentationPage";
 
 function App() {
   const [userId, setUserId] = createSignal("");
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar userId={userId()} />
       <Routes>
-        <Route path={"/"} component={HomePage} />
+        <Route
+          path={"/"}
+          data={userId}
+          component={<HomePage userId={userId()} />}
+        />
         <Show
           when={userId() !== ""}
           fallback={<Route path={"/dash"} component={UnauthorizedPage} />}
         >
           <Route path={"/dash"} component={HomePage} />;
         </Show>
-        <Route path={"/docs"} component={HomePage} />
+        <Route path={"/docs"} component={DocumentationPage} />
         <Route path={"/about"} component={HomePage} />
         <Route path={"*"} component={NotFoundPage} />
       </Routes>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,42 @@
+import { Navbar } from "../components/Navbar/Navbar";
 import { PageBg } from "../components/global/PageBg";
 import { FeatureCard } from "../components/home/FeatureCard";
 
 import featureCardData from "../data/featureCardData.json";
 
-export const HomePage = () => {
+export const HomePage = ({ userId }) => {
   return (
-    <PageBg>
-      <div className='flex flex-col items-center justify-center gap-28 grow py-14'>
-        <div className='flex flex-col items-center gap-5'>
-          <h1 className='text-transparent text-9xl font-julius-sans main-gradient-light bg-clip-text bg-gradient-to-r'>
-            Quotr
-          </h1>
-          <div className='p-1 transition-all group hover:-translate-y-0.5 hover:shadow-2xl main-gradient-light w-fit rounded-xl bg-gradient-to-r hover:from-transparent hover:to-transparent hover:bg-discord'>
-            <button className='flex flex-row items-center gap-5 p-5 transition-all rounded-lg bg-neutral-900 hover:bg-discord'>
-              <img src='/icons/discord.svg' />
-              <p className='text-4xl font-bold leading-none text-white transition-colors font-comfortaa whitespace-nowrap'>
-                Sign In
-              </p>
-            </button>
+    <>
+      <Navbar userId={userId} />
+      <PageBg>
+        <div className="flex flex-col items-center justify-center gap-28 grow py-14">
+          <div className="flex flex-col items-center gap-5">
+            <h1 className="text-transparent text-9xl font-julius-sans main-gradient-light bg-clip-text bg-gradient-to-r">
+              Quotr
+            </h1>
+            <div className="p-1 transition-all group hover:-translate-y-0.5 hover:shadow-2xl main-gradient-light w-fit rounded-xl bg-gradient-to-r hover:from-transparent hover:to-transparent hover:bg-discord">
+              <button className="flex flex-row items-center gap-5 p-5 transition-transform rounded-lg bg-neutral-900 hover:bg-transparent">
+                <img src="/icons/discord.svg" />
+                <p className="text-4xl font-bold leading-none text-white transition-colors font-comfortaa whitespace-nowrap">
+                  Sign In
+                </p>
+              </button>
+            </div>
           </div>
+          <ul className="inline-flex items-center justify-center gap-16 max-w-[100rem] px-10 flex-wrap">
+            {featureCardData.map((x) => {
+              return (
+                <FeatureCard
+                  name={x.name}
+                  icon={x.icon}
+                  description={x.description}
+                  learnMoreUrl={x.learnMoreUrl}
+                />
+              );
+            })}
+          </ul>
         </div>
-        <ul className='inline-flex items-center justify-center gap-16 max-w-[100rem] px-10 flex-wrap'>
-          {featureCardData.map((x) => {
-            return (
-              <FeatureCard
-                name={x.name}
-                icon={x.icon}
-                description={x.description}
-                learnMoreUrl={x.learnMoreUrl}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </PageBg>
+      </PageBg>
+    </>
   );
 };

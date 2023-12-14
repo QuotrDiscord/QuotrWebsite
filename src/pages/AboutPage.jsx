aa6d1ddcae99a2f403bc745d75387d4c453bf296
H-274 <GitHub@jsmarier.com>
2023-12-13 23:54:06 -0500

    Made a basic layout for About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,16 @@ import { PageBg } from "../components/global/PageBg";
 export const AboutPage = () => {
   return (
     <PageBg>
-      <div>a</div>
+      <div className="w-full p-16 mx-auto text-justify text-white bg-black bg-opacity-25 max-w-7xl font-comfortaa">
+        <div className="flex flex-col gap-2 p-10 text-center ">
+          <h1 className="font-julius-sans text-7xl">Quotr</h1>
+          <p className="text-md">My solution to a problem that doesn't exist</p>
+        </div>
+        <div className="flex flex-col gap-2 p-10">
+          <h2 className="text-5xl font-julius-sans">What is Quotr</h2>
+          <p className="pl-16"></p>
+        </div>
+      </div>
     </PageBg>
   );
 };
